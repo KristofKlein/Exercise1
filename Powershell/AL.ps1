45a082168095880ad234107abefe26c0d6388043
Kristof Klein <Kristof.Klein@hansencx.com>
2020-03-10 10:30:37 +0100

    inital commit with an ALGO fitting the container

---
+++ Powershell/AL.ps1
@@ -0,0 +1,13 @@
+Write-Host '###### Update Nav Containerhelper! ######' -ForegroundColor Blue
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Install-NavContainerHelper.ps1')
+
+Write-Host '###### Create Container ######' -ForegroundColor Blue
+New-NavContainer -accept_eula `
+                 -containerName 'ALGO' `
+                 -imageName 'mcr.microsoft.com/businesscentral/onprem:15.2.39040.0-w1-ltsc2019' `
+                 -updateHosts `
+                 -locale 'da-DK' `
+                 -isolation 'hyperv'
+
+Write-Host 'Thats all Folks' -ForegroundColor Blue
+
